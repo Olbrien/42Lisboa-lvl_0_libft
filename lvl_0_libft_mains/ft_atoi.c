2111311f9b3b494d83cedbaa99974d8ae2155b95
Olbrien <77759096+Olbrien@users.noreply.github.com>
2021-02-20 14:55:24 +0000

    Update ft_atoi.c

--- lvl_0_libft_mains/ft_atoi.c
+++ lvl_0_libft_mains/ft_atoi.c
@@ -10,36 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int ft_atoi(const char *str)
-{
-	int i;
-	int total;
-	int negative;
-
-	i = 0;
-	total = 0;
-	negative = 1;
-	while (str[i] == ' ' || str[i] == '\f' || str[i] == '\n' || \
-		   str[i] == '\r' || str[i] == '\t' || str[i] == '\v')
-		   {
-			   i++;
-		   }
-	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			negative = -1;
-		i++;
-	}
-	while (str[i] >= '0' && str[i] <= '9')
-	{
-		total = total * 10 + (str[i] - '0');
-		i++;
-	}
-	return (total * negative);
-}
-
 int		main()
 {
 	char a[] = "-1231as23";
