3c37b1234d8b76c20f785e6c19a95446f33d7ab3
Olbrien <77759096+Olbrien@users.noreply.github.com>
2021-04-19 00:20:46 +0100

    Add files via upload

---
+++ lvl_0_libft_new_norm/ft_split_chars_bonus.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_chars_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tisantos <tisantos@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/05 18:54:34 by tisantos          #+#    #+#             */
+/*   Updated: 2021/04/19 00:19:31 by tisantos         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**ft_clear_splitted(char **tab)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+	return (NULL);
+}
+
+static unsigned int	ft_get_nb_strs(char const *s, const char *delimiters)
+{
+	unsigned int	i;
+	unsigned int	nb_strs;
+
+	if (!s[0])
+		return (0);
+	i = 0;
+	nb_strs = 0;
+	while (s[i] && ft_strchr(delimiters, s[i]))
+		i++;
+	while (s[i])
+	{
+		if (ft_strchr(delimiters, s[i]))
+		{
+			nb_strs++;
+			while (s[i] && ft_strchr(delimiters, s[i]))
+				i++;
+			continue ;
+		}
+		i++;
+	}
+	if (!ft_strchr(delimiters, s[i - 1]))
+		nb_strs++;
+	return (nb_strs);
+}
+
+static void	ft_get_next_str(char **next_str, unsigned int *next_str_len,
+					const char *delimiters)
+{
+	unsigned int	i;
+
+	*next_str += *next_str_len;
+	*next_str_len = 0;
+	i = 0;
+	while (**next_str && ft_strchr(delimiters, **next_str))
+		(*next_str)++;
+	while ((*next_str)[i])
+	{
+		if (ft_strchr(delimiters, (*next_str)[i]))
+			return ;
+		(*next_str_len)++;
+		i++;
+	}
+}
+
+char	**ft_split_chars(char const *s, const char *delimiters)
+{
+	char			**tab;
+	char			*next_str;
+	unsigned int	next_str_len;
+	unsigned int	nb_strs;
+	unsigned int	i;
+
+	nb_strs = ft_get_nb_strs(s, delimiters);
+	tab = malloc(sizeof(char *) * (nb_strs + 1));
+	if (tab == NULL)
+		return (NULL);
+	i = 0;
+	next_str = (char *)s;
+	next_str_len = 0;
+	while (i < nb_strs)
+	{
+		ft_get_next_str(&next_str, &next_str_len, delimiters);
+		tab[i] = malloc(sizeof(char) * (next_str_len + 1));
+		if (tab[i] == NULL)
+			return (ft_clear_splitted(tab));
+		ft_strlcpy(tab[i], next_str, next_str_len + 1);
+		i++;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
--- lvl_0_libft_new_norm/libft.h
+++ lvl_0_libft_new_norm/libft.h
@@ -6,7 +6,7 @@
 /*   By: tisantos <tisantos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 18:33:47 by marvin            #+#    #+#             */
-/*   Updated: 2021/02/20 13:37:47 by tisantos         ###   ########.fr       */
+/*   Updated: 2021/04/19 00:18:41 by tisantos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,4 +80,7 @@ char				*ft_itoa_unsigned_longlong(unsigned long long int n);
 char				*ft_strjoinchr(char *s1, char s2);
 long long			ft_pow(int number, unsigned int power);
 char				*ft_itoa_longlong(long long n);
+char				**ft_split_chars(char const *s, const char *delimiters);
+int					ft_strcmp(char *s1, char *s2);
+void				free_array(char **array, int count);
 #endif
